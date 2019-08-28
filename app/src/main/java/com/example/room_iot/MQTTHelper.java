de934205c8938b0c7236a536844c0e124552d7cf
dnp99 <pateldeep1001@gmail.com>
2019-08-28 18:43:45 -0400

    intial commit MQTT working

---
+++ app/src/main/java/com/example/room_iot/MQTTHelper.java
@@ -0,0 +1,6 @@
+package com.example.room_iot;
+
+
+public class MQTTHelper {
+
+}
---
+++ app/src/main/java/com/example/room_iot/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.room_iot;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    MQTTHelper mqttHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tv=findViewById(R.id.tv);
+        startMqtt();
+    }
+
+    private void startMqtt() {
+        final String serverUri = "tcp://m16.cloudmqtt.com:19194";
+
+        final String clientId = MqttClient.generateClientId();
+        final String topic = "room";
+
+        final String username = "lhqwiyin";
+        final String password = "rgaGo8vGawAR";
+        final MqttAndroidClient mqttAndroidClient;
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName(username);
+        mqttConnectOptions.setPassword(password.toCharArray());
+
+        try {
+
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                    try {
+                        Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+                        publishMessage(mqttAndroidClient,"fanOff",0,topic);
+
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("Mqtt", "Failed to connect to: " + serverUri + exception.toString());
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void publishMessage(@NonNull MqttAndroidClient client,
+                               @NonNull String msg, int qos, @NonNull String topic)
+            throws MqttException, UnsupportedEncodingException {
+        byte[] encodedPayload = new byte[0];
+        encodedPayload = msg.getBytes("UTF-8");
+        MqttMessage message = new MqttMessage(encodedPayload);
+        message.setId(5866);
+        message.setRetained(true);
+        message.setQos(qos);
+        client.publish(topic, message);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
